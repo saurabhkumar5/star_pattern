604e89c31b5be30842e1c3e0cd8de97a916046c9
saurabh kumar <saurabhkumar9493@gmail.com>
2024-03-05 14:21:38 +0530

    first commit

---
+++ star.js
@@ -0,0 +1,162 @@
+
+// 1. square pattern
+// for(let i = 1; i<=5;i++){
+     
+//     let row = '';
+//     for(let j = 1; j<=5;j++){
+        
+//      row+='* '
+//     }
+//     console.log(row);
+// }
+
+// 2. increasing  triangle pattern
+
+// for(let i = 1; i<=5;i++){
+     
+//     let row = '';
+//     for(let j = 1; j<=i;j++){
+        
+//      row+='* '
+//     }
+//     console.log(row);
+// }
+
+// 3. decreasing traingle pattern
+
+// for(let i = 1; i<=5;i++){
+     
+//     let row = '';
+//     for(let j = i; j<=5;j++){
+        
+//      row+='* '
+//     }
+//     console.log(row);
+// }
+
+// 4. pahle space last me 1 star phir 2
+
+// for(let i = 1; i<=5; i++){
+//     let row = ""
+//     for(let j = 1; j<= 5 - i; j++){
+//         row+=" "
+//     }
+   
+//     for(let k = 1; k<=i; k++){
+//        row+="*"
+//     }
+//     console.log(row)
+// }
+
+// 5. right side traingle
+
+// for(let i = 0; i<5; i++){
+//     let row = "";
+//     for(let j = 0; j<i; j++){
+//         row+=" ";
+//     }
+
+//     for(let k =0; k<5-i; k++){
+//         row+='*'
+//     }
+//     console.log(row)
+// }
+
+// 6. number pattern  half traingle
+// for(let i = 1; i<=5; i++){
+//     let row = ""
+//     for(j = 1; j<=i; j++){
+//         row+="1 "
+//     }
+//     console.log(row);
+// }
+
+// 7. print half traingle first row 1 second row 2 2
+
+//  for(let i = 1; i<= 5; i++){
+//     let row = '';
+//     for(let j = 1; j<=i; j++){
+//         row+=i+" ";
+//     }
+//     console.log(row);
+//  }
+
+
+// sub string
+
+// let arr = "saurabh";
+// for(let i = 0; i<arr.length; i++){
+//     let bag ="";
+//     for(let j =i; j<arr.length; j++){
+//         bag+=arr[j];
+//         console.log(bag)
+//     }
+// }
+
+
+// 8. character pattern A increasing traingle
+   
+    // for(let i = 1; i<=5; i++){
+    //     let row = ''
+    //    for(let j = 1; j<=i; j++){
+    //        row+="A "
+    //    }
+    //    console.log(row);
+    // }
+
+    // 9. first row A second row B B
+    //  let currentChar = 65;//ascii code for 'A'
+    //  for(let i = 1; i<=5; i++){
+    //     let row = '';
+    //     for (let j = 1; j<=i; j++){
+    //         row+=String.fromCharCode(currentChar)+' ';
+    //     }
+    //     console.log(row);
+    //     currentChar++;
+    //  }
+ 
+
+    // 10. first row A second row B C third row D E F
+
+    // let currentChar = 65;
+    // for (let i = 1; i<=5; i++){
+    //     let row = '';
+    //     for(let j = 1; j<=i; j++){
+    //         row+= String.fromCharCode(currentChar) + ' ';
+    //         currentChar++;
+    //     }
+    //     console.log(row);
+    // }
+
+    // 11 arithmetic series
+
+    // 1+2+3+4+5+...N
+
+    // let sum = 0;
+    // for(let i = 1; i<=5;i++){
+    //     sum = sum+i;
+    // }
+    // console.log(sum)
+
+    // 2+4+6+8
+   
+    //  let sum = 0;
+    // for(let i = 2; i<=8;i+=2){
+    //     sum = sum+i;
+    // }
+    // console.log(sum);
+
+    // 9+13+17+...
+    // 10+9+8+...1
+
+    // sum power
+
+    // x1+x2+x3+x4+...N
+
+    // let sum = 0;
+    // for(let i = 1; i<=5; i++){
+    //   sum = sum+Math.pow(2,i)
+    // }
+    // console.log(sum)
+
+    
